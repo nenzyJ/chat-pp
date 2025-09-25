8c8d8f25681d2b1300feac0bd5a0d2019347503c
nenzyJ <jamside0807@gmail.com>
2025-09-25 19:12:51 +0300

    fixed-message-controller

--- backend/src/controllers/message.conroller.js
+++ backend/src/controllers/message.conroller.js
@@ -41,6 +41,20 @@ export const sendMessage = async (req, res) => {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
+
+    if(!text && !image) {
+         return res.status(400).json({ message: "Message text or image is required" });
+    }
+    if (senderId.equals(receiverId)){
+        return res.status(400).json({ message: "You cannot send message to yourself" });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+        return res.status(404).json({ message: "Receiver not found" });
+    }
+
+
+
     let imageUrl;
     if (image) {
       const uploadResponse = await cloudinary.uploader.upload(image);
