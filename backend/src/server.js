3596e666a283a178ed2055d95d9ab09ceae44319
nenzyJ <jamside0807@gmail.com>
2025-09-27 17:43:28 +0300

    page completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,14 +6,18 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
+import cors from "cors"
 const app = express();
 
 const __dirname = path.resolve();
 
-const PORT = ENV.PORT;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // to parse json body
+app.use(cors({
+  origin: ENV.CLIENT_URL,
+  credentials: true,
+}))
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,39 @@
 import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "doue", _id: 123, age: 21},
-    isLoading: false,
-    isLoggedIn: false,
-    login: () => {
-        console.log("loginned in")
-        set({isLoggedIn: true, isLoading: true})
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
+
+    checkAuth: async () => {
+        try {
+            const res =  axiosInstance.get("/auth/check")
+            set({authUser: res.data})
+        } catch (error) {
+            console.log("Error checking auth:", error)
+            set({authUser: null})
+        } finally {
+            set({isCheckingAuth: false})
+        }
+    },
+
+
+    signup: async(data) => {
+        set({isSigningUp: true})
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({authUser: res.data})
+
+            //toast success
+            toast.success("Account created successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+            
+        } finally {
+            set({isSigningUp: false})
+        }
     }
-}))
+
+ }))
